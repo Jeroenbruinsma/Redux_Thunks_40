c6603b5fa17f7e21bb65d991e55c0e2b63605209
Reinoptland <reinoptland@gmail.com>
2020-06-05 11:30:47 +0200

    Make request to api in thunk

--- src/store/postDetails/actions.js
+++ src/store/postDetails/actions.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
+
 // thunk that has a parameter: id
 export function fetchPostById(id) {
-  return function thunk(dispatch, getState) {
+  return async function thunk(dispatch, getState) {
     console.log("WHAT POST DO WE FETCH", id);
+    const response = await axios.get(`${API_URL}/posts/${id}`);
+    console.log(response.data);
   };
 }
