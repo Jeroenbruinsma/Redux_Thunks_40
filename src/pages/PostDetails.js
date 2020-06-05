4878a76e0ae099607360270e25466d46699326a1
Reinoptland <reinoptland@gmail.com>
2020-06-05 11:53:05 +0200

    Wrote a selector for postDetails, use it in component

--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import { fetchPostById } from "../store/postDetails/actions";
+import { selectPost } from "../store/postDetails/selectors";
 import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function PostDetails() {
   const { id } = useParams();
+  const post = useSelector(selectPost);
+  console.log("DO WE HAVE OUR POST?", post);
   const dispatch = useDispatch();
   useEffect(() => {
     // console.log("EFFECT", fetchPostById, "POST ID:", id);
